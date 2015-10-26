6f8deebc685752113e7fb1ad10b467201a42fd82
git-jhchang <jh_chang@trend.com.tw>
2015-10-26 23:39:31 +0800

    test1 - red light

---
+++ PotterShoppingCart/PotterShoppingCartTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCart
+{
+    [TestClass]
+    public class PotterShoppingCartTest
+    {
+        [TestMethod]
+        public void Test1_第一集買一本_其他都沒買_價格應為100乘以1等於100元()
+        {
+            // arrange
+            var bought_books = new List<Book>
+            {
+                new Book { name = "vol 1", price = 100 }
+            };
+            var target = new ShoppingCart(bought_books);
+            var expected = 100;
+
+            // act
+            var actual = target.CountPrice();
+
+            // asert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ PotterShoppingCart/ShoppingCart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart
+{
+    public class ShoppingCart
+    {
+        private List<Book> _bought_books;
+
+        public ShoppingCart(List<Book> bought_books)
+        {
+            // TODO: Complete member initialization
+            this._bought_books = bought_books;
+        }
+
+
+        internal object CountPrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Book
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+    }
+}
